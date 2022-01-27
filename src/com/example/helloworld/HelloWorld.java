45d89230f97409ef7f839ee1dc96a980e81c02ad
marina12n <m.d.fedorchuk@gmail.com>
2022-01-27 21:43:24 +0200

    SHO

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!text");
+        System.out.println("Hello World");
     }
 }
