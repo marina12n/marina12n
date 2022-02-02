c939f69b3880ddc728517398cfeed87cb8f6e0e9
marina12n <m.d.fedorchuk@gmail.com>
2022-02-02 22:43:01 +0200

    robota

---
+++ Day.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+public class Day {
+    public static String weekday(string Wednesday){
+        String weekday = "Wednesday"
+            if (weekday ==Wednesday){
+                result = "Work work work";
+
+                if (Weekday==Monday||Weekday==Tuesday||Weekday==Wednesday)
+                    System.out.println"(Work work work)";
+                else if (Weekday==Thursday||Weekday==Friday)
+                    System.out.println"(Weekend is coming)";
+            }
+    }
+}
+
--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.example.helloworld;
 
+import java.time.LocalDate;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
